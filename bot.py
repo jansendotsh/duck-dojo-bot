6e80862b2df4e57725d2ce4a8aae42319d16a324
Garrett Jansen <garrett@jansen.sh>
2019-07-08 12:05:05 -0400

    Re-adding bot.py without history

---
+++ bot.py
@@ -0,0 +1,32 @@
+from vars.private import telegram_api
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()    
+    url = contents['url']
+    return url
+
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
+def boop(bot, update):
+    url = get_image_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+        updater = Updater(telegram_api)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('boop',boop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
