f73f19e280673b238b4b9255c63bd03996b13095
Garrett Jansen <garrett@jansen.sh>
2020-02-10 20:41:35 -0500

    roland you big dumb baby

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def boop(bot, update):
     bot.send_photo(chat_id=chat_id, photo=url)
 
 def roll(bot, update):
-    rollurl = 'https://dojo.burning.cloud/roll/' + random.randint(1,2)
+    rollurl = 'https://dojo.burning.cloud/roll/' + random.randint(1,2) + ".jpg"
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=rollurl)
 
