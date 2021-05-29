cffafa0ac1074c9fd9a4660bbca19714fd504179
jansendotsh <garrett@jansen.sh>
2021-05-28 21:16:04 -0700

    Remodel for Docker

--- bot.py
+++ bot.py
@@ -1,33 +1,14 @@
-from vars.private import telegram_api
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-import requests
-import re
+from telegram.ext import Updater, CommandHandler
+import logging
+import os
 import random
+import requests
 
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()    
-    url = contents['url']
-    return url
-
-def get_image_url():
-    allowed_extension = ['jpg','jpeg','png']
-    file_extension = ''
-    while file_extension not in allowed_extension:
-        url = get_url()
-        file_extension = re.search("([^.]*)$",url).group(1).lower()
-    return url
-
-def boop(bot, update):
-    url = get_image_url()
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
-
-def roll(bot, update):
+def roll(update, context):
     url = f'https://dojo.burning.cloud/roll/{random.randint(1,11)}.jpg'
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=url)
 
-def chiweeners(bot, update):
+def chiweeners(update, context):
     d20 = random.randint(1,20)
     if d20 < 5:
         d20 = random.randint(1,20)
@@ -37,21 +18,20 @@ def chiweeners(bot, update):
             url = f'http://scuttle.nithingpole.com/rand{random.randint(1,5)}.png'
     else:
         url = f'http://scuttle.nithingpole.com/doge{random.randint(1,44)}.png'
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=url)
 
-def lucky_hit(bot, update):
+def lucky_hit(update, context):
     url = "http://scuttle.nithingpole.com/RYO.txt"
     ryo_quotes = requests.get(url).content.decode().split("\n")
     random.shuffle(ryo_quotes)
     ryo_quote = random.choice(ryo_quotes)
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text=ryo_quote)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=ryo_quote)
 
 def main():
-    updater = Updater(telegram_api)
+    telegram_api = os.environ['TELEGRAM_TOKEN']
+    updater = Updater(token=telegram_api, use_context=True)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('boop',boop))
     dp.add_handler(CommandHandler('roll',roll))
     dp.add_handler(CommandHandler('chiweeners',chiweeners))
     dp.add_handler(CommandHandler('lucky_hit',lucky_hit))
