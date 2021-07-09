25ee1c8dffc07f00d0e8c86c0d1e92c4a8699719
Tim Dawson <tjdawson1917@gmail.com>
2021-07-09 18:10:18 -0400

    lie bot

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import random
 import requests
 import json
 from datetime import datetime
+from bs4 import BeautifulSoup
+from faker import Faker
 
 def roll(update, context):
     url = f'https://dojo.burning.cloud/roll/{random.randint(1,11)}.jpg'
@@ -41,6 +43,50 @@ def on_this_day_bot(update, context):
     result = str(result_year) + ": " + result + f'\nsource: {result_source}'
     context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
+def lie_bot(update, context):
+    fake = Faker()
+    Faker.seed(random.randint(0,1312+1))
+
+    url = f"https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+    seeds = []
+    for i in range(0,7):
+        request = requests.get(url).content
+        request = json.loads(request)
+        seeds.append(request)
+    random.shuffle(seeds)
+
+    years = ['65,000,000 BC', '12,000 BC', '600 AD', '1000 AD', '1999 AD', '2300 AD']
+    for i in range(0,10):
+        random_year = random.randint(0,3031) 
+        years.append(str(random_year) + " AD")
+
+    year = years.pop(random.randrange(len(years)))
+    day = fake.day_of_week()
+    date =  fake.day_of_month()
+    month = fake.month_name()
+
+    title = seeds.pop(random.randrange(len(seeds)))
+    description = seeds.pop(random.randrange(len(seeds)))
+    photo = seeds.pop(random.randrange(len(seeds)))
+
+    title = title['title']
+    description = description['extract_html']
+    photo = photo['originalimage']['source']
+
+    soup=BeautifulSoup(description, features="html.parser")
+    for x in soup.find_all('b'):
+        x.string=title
+    description=soup.get_text()
+
+    suffixes = ['th', 'st', 'nd', 'rd']
+    suffix = suffixes.pop(random.randrange(len(suffixes)))
+
+    result = description + "\n" + f"Today is {day}, {month} {date}{suffix}, {year}." 
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo)
+
 def main():
     telegram_api = os.environ['TELEGRAM_TOKEN']
     updater = Updater(token=telegram_api, use_context=True)
@@ -50,6 +96,7 @@ def main():
     dp.add_handler(CommandHandler('chiweeners',chiweeners))
     dp.add_handler(CommandHandler('lucky_hit',lucky_hit))
     dp.add_handler(CommandHandler('on_this_day',on_this_day_bot))
+    dp.add_handler(CommandHandler('lie_bot',lie_bot))
     updater.start_polling()
     updater.idle()
 
